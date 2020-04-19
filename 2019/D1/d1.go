40847e6aca396b860ace4494626e01180b3d4631
Aayush Gupta <aayu.gupta.96@gmail.com>
2020-04-19 22:02:35 +0530

    setup more
    Signed-off-by: Aayush Gupta <aayu.gupta.96@gmail.com>

--- 2019/D1/d1.go
+++ 2019/D1/d1.go
@@ -1,22 +1,23 @@
-package main
+package d1
 
 import (
+	"AdventOfCode/utils"
+	"fmt"
 	"io/ioutil"
 	"math"
-
-	"../../utils"
 )
 
-func main() {
+// Compute -> Do whatwe
+func Compute() {
 
 	/*
 		Fuel required(module) = floor(M_mod / 3) - 2
 	*/
 
-	input, err := ioutil.ReadFile("input.txt")
+	input, err := ioutil.ReadFile("2019/D1/input.txt")
 	utils.Check(err)
-	str_input := string(input)
-
+	strInput := string(input)
+	fmt.Println(strInput)
 }
 
 func getFuelForMass(mass int32) int32 {
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "AdventOfCode/2019/d1"
+
+func main() {
+
+	// Day 1 Part 1
+	d1.Compute()
+}
