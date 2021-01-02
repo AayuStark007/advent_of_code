f524a8aafb998a3d4e45e10764e47639d201f115
Aayush Gupta <aayu.gupta.96@gmail.com>
2021-01-02 16:51:30 +0530

    move common fn to utils

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -1,7 +1,4 @@
-use std::collections::HashMap;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use util::aoc::{find_pair, read_lines_str};
 
 fn main() {
     println!("Reading input");
@@ -35,27 +32,4 @@ fn main() {
             }
         }
     }
-}
-
-fn find_pair(list: &Vec<i32>, sum: &i32) -> Option<(i32, i32)> {
-    let mut diff = HashMap::<i32, i32>::new();
-    for num in list.iter() {
-        if let Some(_) = diff.get(num) {
-            return Some((num.clone(), sum - num));
-        }
-        diff.insert(sum - num, 1);
-    }
-
-    return None;
-}
-
-fn read_lines_str<P>(filename: P) -> io::Result<Vec<String>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file)
-        .lines()
-        .collect::<Result<_, _>>()
-        .unwrap())
 }
--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,7 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub mod aoc {
+    use std::collections::HashMap;
+    use std::fs::File;
+    use std::io::{self, BufRead};
+    use std::path::Path;
+    pub fn find_pair(list: &Vec<i32>, sum: &i32) -> Option<(i32, i32)> {
+        let mut diff = HashMap::<i32, i32>::new();
+        for num in list.iter() {
+            if let Some(_) = diff.get(num) {
+                return Some((num.clone(), sum - num));
+            }
+            diff.insert(sum - num, 1);
+        }
+        return None;
+    }
+    pub fn read_lines_str<P>(filename: P) -> io::Result<Vec<String>>
+    where
+        P: AsRef<Path>,
+    {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file)
+            .lines()
+            .collect::<Result<_, _>>()
+            .unwrap())
     }
 }
