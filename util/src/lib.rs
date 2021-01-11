c04a33834617df49f6219800609c4d0681553946
Aayush Gupta <aayu.gupta.96@gmail.com>
2021-01-11 20:07:43 +0530

    file reader utils in buffered mode

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -3,6 +3,8 @@ pub mod aoc {
     use std::fs::File;
     use std::io::{self, BufRead};
     use std::path::Path;
+
+    use io::{BufReader, Lines};
     pub fn find_pair(list: &Vec<i32>, sum: &i32) -> Option<(i32, i32)> {
         let mut diff = HashMap::<i32, i32>::new();
         for num in list.iter() {
@@ -13,14 +15,19 @@ pub mod aoc {
         }
         return None;
     }
-    pub fn read_lines_str<P>(filename: P) -> io::Result<Vec<String>>
+
+    pub fn read_lines<P>(filename: P) -> io::Result<Lines<BufReader<File>>>
     where
         P: AsRef<Path>,
     {
         let file = File::open(filename)?;
-        Ok(io::BufReader::new(file)
-            .lines()
-            .collect::<Result<_, _>>()
-            .unwrap())
+        Ok(io::BufReader::new(file).lines())
+    }
+
+    pub fn read_lines_str<P>(filename: P) -> io::Result<Vec<String>>
+    where
+        P: AsRef<Path>,
+    {
+        Ok(read_lines(filename)?.collect::<Result<_, _>>().unwrap())
     }
 }
