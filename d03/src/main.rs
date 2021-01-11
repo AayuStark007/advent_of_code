cd72e4d17e6230cc4c22c2b367aad429216ff470
Aayush Gupta <aayu.gupta.96@gmail.com>
2021-01-11 20:38:06 +0530

    Day 3, Part 1 solution

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -1,3 +1,27 @@
+use util::aoc::read_lines;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Reading input");
+
+    if let Ok(lines) = read_lines("input.txt") {
+        let mut pos: usize = 0;
+        let mut n_tree: usize = 0;
+        for line in lines {
+            let line_str = match line {
+                Ok(line_str) => line_str,
+                Err(e) => panic!(e),
+            };
+            if advance_pos(&line_str, &mut pos) {
+                n_tree += 1;
+            }
+        }
+
+        println!("Part 1: {}", n_tree);
+    }
+}
+
+fn advance_pos(line: &String, pos: &mut usize) -> bool {
+    let result = line.chars().nth(*pos).unwrap() == '#';
+    *pos = (*pos + 3) % line.len();
+    return result;
 }
